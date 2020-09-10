a63444fb3833c2a3c8a71c20d0cfd0df3f088ef8
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-10 20:05:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+class Polygon {
+  constructor(array){
+  this.array = array
+  } 
+  
+  get countSides() { 
+    
+  }
+} 
+
+class Triangle extends Polygon {
+  
+}
+
+class Square extends Polygon {
+
+}
