1cc553e8fe4c88f3d9a868cae7781cbf94f90904
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-10 20:15:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,27 +19,32 @@ class Polygon {
 }
 
 class Triangle extends Polygon {
-  
-  get isValid() { 
-    if (this.sides.length == 3) 
-    true 
-    else 
-    false 
+
+  get isValid() {
+    if ( !Array.isArray( this.sides ) ) return;
+    if ( this.count !== 3 ) return;
+    let side1 = this.sides[ 0 ]
+    let side2 = this.sides[ 1 ]
+    let side3 = this.sides[ 2 ]
+    return ( ( side1 === side2 ) && ( side1 === side3 ) && ( side2 === side3 ) )
   }
-  
 }
 
 class Square extends Polygon {
-  
-  get isValid() { 
-    if (this.sides.length == 4) 
-    true 
-    else 
-    false 
-  } 
-  
-  get area() { 
-  
-  } 
 
+  get isValid() {
+    if ( !Array.isArray( this.sides ) ) return;
+    if ( this.count !== 4 ) return;
+    let side1 = this.sides[ 0 ]
+    let side2 = this.sides[ 1 ]
+    let side3 = this.sides[ 2 ]
+    let side4 = this.sides[ 3 ]
+    return ( ( side1 === side2 ) && ( side1 === side3 ) && ( side1 === side4 ) )
+  }
+
+  get area() {
+    if ( !Array.isArray( this.sides ) ) return;
+    if ( this.count !== 4 ) return;
+    return this.sides[ 0 ] * this.sides[ 0 ]
+  }
 }
